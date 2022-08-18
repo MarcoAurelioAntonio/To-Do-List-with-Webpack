ef53783242cbcda4d820898863372f3b60772b71
Marcus Aurelius <99001924+MarcoAurelioAntonio@users.noreply.github.com>
2022-08-18 00:48:46 -0300

    basic html y style

--- src/index.js
+++ src/index.js
@@ -1,21 +1,12 @@
-import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
+/* import _ from 'lodash'; */
+/* import './style.css'; */
 
-function component() {
+/* function component() {
   const element = document.createElement('div');
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
-
-  // Add the image to our existing div.
-  const myIcon = new Image();
-  myIcon.src = Icon;
-
-  element.appendChild(myIcon);
-
-  return element;
 }
 
-document.body.appendChild(component());
+document.body.appendChild(component()); */
