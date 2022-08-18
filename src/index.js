46fa6feb629ddcf3b308aa98649d066578eeedeb
Marcus Aurelius <99001924+MarcoAurelioAntonio@users.noreply.github.com>
2022-08-18 12:03:53 -0300

    firt part done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,50 @@
 /* import _ from 'lodash'; */
 /* import './style.css'; */
 
-/* function component() {
-  const element = document.createElement('div');
+const listArray = [
+  {
+    description: 'Help to mi Son to do hes homework',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'Go to the grocery store',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Go for a run',
+    completed: false,
+    index: 2,
+  },
+];
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-}
-
-document.body.appendChild(component()); */
+const list = document.getElementById('list');
+window.addEventListener('load', () => {
+  for (let i = 0; i < listArray.length; i += 1) {
+    // task div
+    const div = document.createElement('div');
+    div.classList.add('divTask');
+    // check
+    const check = document.createElement('input');
+    check.setAttribute('type', 'checkbox');
+    check.classList.add('check');
+    // descrption
+    const p = document.createElement('p');
+    p.textContent = `${listArray[i].description}`;
+    p.classList.add('p');
+    // edit button TRICOLON
+    const edit = document.createElement('button');
+    edit.classList.add('edit');
+    edit.textContent = '⁝';
+    // appending p, check and tricolon
+    div.appendChild(check);
+    div.appendChild(p);
+    // div container
+    const divCont = document.createElement('div');
+    divCont.classList.add('divCont');
+    divCont.appendChild(div);
+    divCont.appendChild(edit);
+    list.appendChild(divCont);
+  }
+});
